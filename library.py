390bf7ea7ed6de613533916c3cf199b52a4eb2f2
ruimin-z <ruiminz1@gmail.com>
2023-10-22 04:23:19 -0700

    find_random_state() titanic_transformer customer_transformer fitted_pipeline

--- library.py
+++ library.py
@@ -1,11 +1,19 @@
 import pandas as pd
 import numpy as np
 import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier  # the KNN model
+from sklearn.metrics import f1_score  # typical metric used to measure goodness of a model
 
 sklearn.set_config(transform_output="pandas")  # says pass pandas tables through pipeline instead of numpy matrices
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 from sklearn.impute import KNNImputer
+
+import subprocess
+import sys
+# subprocess.call([sys.executable, '-m', 'pip', 'install', 'category_encoders'])  #replaces !pip install
+import category_encoders as ce
 
 
 class CustomMappingTransformer(BaseEstimator, TransformerMixin):
@@ -139,6 +147,9 @@ class CustomOHETransformer(BaseEstimator, TransformerMixin):
 
 
 class CustomSigma3Transformer(BaseEstimator, TransformerMixin):
+    """
+    Clip outliers using 3 Sigma method through standard deviation and mean
+    """
     def __init__(self, target_column):
         self.target_column = target_column
         self.minb = None
@@ -174,6 +185,9 @@ class CustomSigma3Transformer(BaseEstimator, TransformerMixin):
 
 
 class CustomTukeyTransformer(BaseEstimator, TransformerMixin):
+    """
+    Clip outliers using Tukey method through quartiles and median
+    """
     def __init__(self, target_column, fence='outer'):
         assert fence in ['inner', 'outer']
         self.fence = fence
@@ -214,6 +228,10 @@ class CustomTukeyTransformer(BaseEstimator, TransformerMixin):
 
 
 class CustomRobustTransformer(BaseEstimator, TransformerMixin):
+    """
+    This class scales a given column values in [0,1] using Robust Scaler.
+    Formula: value_new = (value – median) / iqr #iqr = q3-q1
+    """
     def __init__(self, column):
         self.column = column
         self.iqr, self.med = None, None
@@ -249,21 +267,76 @@ class CustomRobustTransformer(BaseEstimator, TransformerMixin):
         return result
 
 
-if __name__ == '__main__':
-    titanic_transformer = Pipeline(steps=[
-        ('map_gender', CustomMappingTransformer('Gender', {'Male': 0, 'Female': 1})),
-        ('map_class', CustomMappingTransformer('Class', {'Crew': 0, 'C3': 1, 'C2': 2, 'C1': 3})),
-        ('ohe_joined', CustomOHETransformer(target_column='Joined')),
-        ('tukey_age', CustomTukeyTransformer(target_column='Age', fence='outer')),
-        ('tukey_fare', CustomTukeyTransformer(target_column='Fare', fence='outer')),
-        ('scale_age', CustomRobustTransformer('Age')),
-        ('scale_fare', CustomRobustTransformer('Fare')),
-        # add step below
-        ('knn', KNNImputer(n_neighbors=10, weights='uniform', add_indicator=False))
-    ], verbose=True)
+def find_random_state(features_df, labels, n=200):
+    """Takes a dataframe in and runs the variance code on it.
+    It should return the value to use for the random state in the split method.
+    """
+    model = KNeighborsClassifier(n_neighbors=5)  # instantiate with k=5.
+    var = []  # collect test_error/train_error where error based on F1 score
 
-    # url = 'https://raw.githubusercontent.com/ruimin-z/mlops/main/datasets/titanic_trimmed.csv'  # trimmed version
-    # titanic_trimmed = pd.read_csv('datasets/titanic_trimmed.csv')
-    # titanic_features = titanic_trimmed.drop(columns='Survived')
-    # transformed_df = titanic_transformer.fit_transform(titanic_features)
-    # print(transformed_df)
+    for i in range(1, n):
+        train_X, test_X, train_y, test_y = train_test_split(features_df, labels, test_size=0.2, shuffle=True,
+                                                            random_state=i, stratify=labels)
+        model.fit(train_X, train_y)  # train model
+        train_pred = model.predict(train_X)  # predict against training set
+        test_pred = model.predict(test_X)  # predict against test set
+        train_f1 = f1_score(train_y, train_pred)  # F1 on training predictions
+        test_f1 = f1_score(test_y, test_pred)  # F1 on test predictions
+        f1_ratio = test_f1 / train_f1  # take the ratio
+        var.append(f1_ratio)
+
+    rs_value = sum(var) / len(var)  # get average ratio value
+    idx = np.array(abs(var - rs_value)).argmin()  # find the index of the smallest value
+    return idx
+
+
+
+titanic_variance_based_split = 107 # random state that provides best average
+customer_variance_based_split = 113 # random state that provides best average
+titanic_transformer = Pipeline(steps=[
+    ('map_gender', CustomMappingTransformer('Gender', {'Male': 0, 'Female': 1})),
+    ('map_class', CustomMappingTransformer('Class', {'Crew': 0, 'C3': 1, 'C2': 2, 'C1': 3})),
+    ('target_joined', ce.TargetEncoder(cols=['Joined'],
+                                       handle_missing='return_nan',  # will use imputer later to fill in
+                                       handle_unknown='return_nan'  # will use imputer later to fill in
+                                       )),
+    ('tukey_age', CustomTukeyTransformer(target_column='Age', fence='outer')),
+    ('tukey_fare', CustomTukeyTransformer(target_column='Fare', fence='outer')),
+    ('scale_age', CustomRobustTransformer('Age')),  # from chapter 5
+    ('scale_fare', CustomRobustTransformer('Fare')),  # from chapter 5
+    ('imputer', KNNImputer(n_neighbors=5, weights="uniform", add_indicator=False))  # from chapter 6
+], verbose=True)
+
+customer_transformer = Pipeline(steps=[
+    ('map_os', CustomMappingTransformer('OS', {'Android': 0, 'iOS': 1})),
+    ('target_isp', ce.TargetEncoder(cols=['ISP'],
+                                    handle_missing='return_nan',  # will use imputer later to fill in
+                                    handle_unknown='return_nan'  # will use imputer later to fill in
+                                    )),
+    ('map_level', CustomMappingTransformer('Experience Level', {'low': 0, 'medium': 1, 'high': 2})),
+    ('map_gender', CustomMappingTransformer('Gender', {'Male': 0, 'Female': 1})),
+    ('tukey_age', CustomTukeyTransformer('Age', 'inner')),  # from chapter 4
+    ('tukey_time spent', CustomTukeyTransformer('Time Spent', 'inner')),  # from chapter 4
+    ('scale_age', CustomRobustTransformer('Age')),  # from 5
+    ('scale_time spent', CustomRobustTransformer('Time Spent')),  # from 5
+    ('impute', KNNImputer(n_neighbors=5, weights="uniform", add_indicator=False)),
+], verbose=True)
+
+
+'''Fitted Transformer'''
+# fitted_pipeline = titanic_transformer.fit(X_train, y_train)  # notice just fit method called
+# import joblib
+# joblib.dump(fitted_pipeline, 'fitted_pipeline.pkl')
+
+
+'''Read titanic_trimmed.csv File'''
+# url = 'https://raw.githubusercontent.com/ruimin-z/mlops/main/datasets/titanic_trimmed.csv'  # trimmed version
+# titanic_trimmed = pd.read_csv('datasets/titanic_trimmed.csv')
+# titanic_features = titanic_trimmed.drop(columns='Survived')
+# transformed_df = titanic_transformer.fit_transform(titanic_features)
+
+'''Read customer.csv File'''
+# url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQPM6PqZXgmAHfRYTcDZseyALRyVwkBtKEo_rtaKq_C7T0jycWxH6QVEzTzJCRA0m8Vz0k68eM9tDm-/pub?output=csv'
+# customers_df = pd.read_csv(url)
+# customers_trimmed = customers_df.drop(columns='ID')  #this is a useless column which we will drop early
+# customers_trimmed = customers_trimmed.drop_duplicates(ignore_index=True)  #get rid of any duplicates
